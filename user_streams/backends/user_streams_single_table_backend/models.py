f7436111080eaf7287b0de781f9876f7b2843d8c
Javier Aguirre <contacto@javaguirre.net>
2013-07-12 12:20:14 +0200

    Small tweak to be able to use customized django 1.5 users with the package

--- user_streams/backends/user_streams_single_table_backend/models.py
+++ user_streams/backends/user_streams_single_table_backend/models.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class StreamItem(models.Model):
-
-    user = models.ForeignKey('auth.User', related_name='+')
+    user = models.ForeignKey(AUTH_USER_MODEL, related_name='+')
     content = models.TextField()
     created_at = models.DateTimeField()
 
